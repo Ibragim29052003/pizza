7a4885060fc968af48af53cf146f333c986c4f3e
Ibragim <ibragardwich@gmail.com>
2025-10-23 15:42:01 +0300

    feat(component): creation and connection of components, preprocessor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import "./scss/app.scss";
+import Header from "./copmonents/Header";
+import Categories from "./copmonents/Categories";
+import Sort from "./copmonents/Sort";
+import PizzaBlock from "./copmonents/PizzaBlock";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello1234543</h1>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Чизбургер-пицца" price={395}/>
+            <PizzaBlock title="Сырная" price={450}/>
+            <PizzaBlock title="Креветки по-азиатски" price={290}/>
+            <PizzaBlock title="Сырный цыпленок" price={385}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
