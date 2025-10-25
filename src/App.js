ecdd256ddf95156d0bdf4a06af7c606d8c324739
Ibragim <ibragardwich@gmail.com>
2025-10-25 16:47:36 +0300

    feat(state): the problem of managing the state of various elements within a single component has been solved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import "./scss/app.scss";
 import Header from "./copmonents/Header";
 import Categories from "./copmonents/Categories";
 import Sort from "./copmonents/Sort";
 import PizzaBlock from "./copmonents/PizzaBlock";
+import pizzas from "./assets/pizzas.json";
+
+import "./scss/app.scss";
 
 function App() {
   return (
@@ -17,15 +19,18 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock title="Чизбургер-пицца" price={395}/>
-            <PizzaBlock title="Сырная" price={450}/>
-            <PizzaBlock title="Креветки по-азиатски" price={290}/>
-            <PizzaBlock title="Сырный цыпленок" price={385}/>
+            {/* <PizzaBlock title="Чизбургер-пицца" price={395} />
+            <PizzaBlock title="Сырная" price={450} />
+            <PizzaBlock title="Креветки по-азиатски" price={290} />
+            <PizzaBlock title="Сырный цыпленок" price={385} /> */}
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj} key={obj.id}></PizzaBlock>
+            ))}
           </div>
         </div>
       </div>
     </div>
-  );
+  );     
 }
 
 export default App;
--- src/copmonents/Categories.jsx
+++ src/copmonents/Categories.jsx
@@ -1,13 +1,42 @@
+import { useState } from "react";
+
 export default function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  function onClickCategory(index) {
+    setActiveIndex(index);
+  }
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {/* <li
+          // обернули в анонимную функцию, чтобы onClick не вызывал сразу же функцию onClickCategory
+          // иначе будет бесконечный ререндер
+          onClick={() => onClickCategory(0)}
+          className={activeIndex === 0 ? "active" : ""}
+        >
+          Все
+        </li> */}
+
+        {categories.map((children, index) => (
+          <li
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? "active" : ""}
+            key={index}
+          >
+            {children}
+          </li>
+        ))}
       </ul>
     </div>
   );
